c0f4f09c3bbc5876d7fb89fc53f96f373a38bbf7
kjk71 <kks007159@nqver.com>
2023-09-26 17:04:11 +0900

    NumSorting

--- src/NumSorting/NumSorting.java
+++ src/NumSorting/NumSorting.java
@@ -1,8 +1,12 @@
 package NumSorting;
 import java.util.*;
 
+/* N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오. */
+
 public class NumSorting {
 	public static void main(String[] args) {
+		
+		/* 이중 for문을 사용헀으며 처음 값다 다음값을 비교해가며 작은 수를 앞에 저장하고 큰 값을 뒤에 넣어주는 것을 반복하며 정렬하도록 만들었다.*/
 		
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
